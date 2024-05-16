2562d932986ccf0886b43b4f33f30decfd68f929
Daniel <daniel.argitek@gmail.com>
2024-05-16 21:36:58 +0200

    +

--- skeletonDaniRodriguez/src/index.js
+++ skeletonDaniRodriguez/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/app';
+import { BrowserRouter, Route, link } from "react-router-dom";
 
 ReactDOM.render(
-  <App />
-  , document.querySelector('.app-wrapper'));
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector(".app-wrapper")
+);
